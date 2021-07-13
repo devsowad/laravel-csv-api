ae820e09f8b2b63fb660e3b168636dd7b78e8654
Sowad <sowad1035@gmail.com>
2021-07-13 16:23:37 +0600

    Adding test route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return 'This is working';
+});
+
 // Route::get('/', [CSVController::class, 'home'])->name('upload');
 // Route::post('/', [CSVController::class, 'upload'])->name('upload');
 // Route::get('/batch/{id}', [CSVController::class, 'batch'])->name('batch');
